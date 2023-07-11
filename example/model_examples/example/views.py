416f0403cddf828b7c857381e2e58f3d09dd1ddf
fvilcaqui <fvilcaqui@unsa.edu.pe>
2023-07-11 00:25:44 -0500

    Terminado video de gmail

--- example/model_examples/example/views.py
+++ example/model_examples/example/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
+def index(request):
+    send_mail('Hello from PrettyPrinted',
+    'Hello there. This is an automated message.',
+    'superlogansito@gmail.com',
+    ['haropap431@meogl.com'],
+    fail_silently=False)
+
+    return render(request, 'send/index.html')
